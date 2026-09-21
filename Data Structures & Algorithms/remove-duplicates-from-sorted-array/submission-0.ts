6f7edb007a568e26cd578bc07e934ba4d4d886c7
Kaleab Dereje <kaleabdereje388@gmail.com>
2026-09-21 23:34:10 +0300

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums: number[]): number {
+        if (nums.length === 0) return 0;
+
+    let write = 1;
+
+    for (let read = 1; read < nums.length; read++) {
+        
+        if (nums[read] !== nums[write - 1]) {
+            nums[write] = nums[read];
+            write++;
+        }
+    }
+
+    return write;
+    }
+}
